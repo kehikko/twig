ee71402bdda3f32f2c2f8b489e3c1e6424b5fcf2
Antti Partanen <aehparta@iki.fi>
2018-12-10 21:05:46 +0200

    twig works

--- twig.php
+++ twig.php
@@ -1,6 +1,43 @@
 <?php
 
+function twig_init(array $extra_paths = [])
+{
+    static $twig = null;
+    if ($twig !== null) {
+        return $twig;
+    }
+
+    $templates_vendor = tool_system_find_files(['views'], [cfg(['paths', 'vendor'])], 2, true);
+    $templates_routes = tool_system_find_files(['views'], [cfg(['paths', 'routes'])], 1, true);
+    $templates        = array_merge($templates_vendor, $templates_routes, $extra_paths);
+    array_unshift($templates, cfg(['paths', 'views']));
+
+    $twig_loader = new Twig_Loader_Filesystem($templates);
+    $config      = cfg('twig');
+    if (!$config) {
+        $config = array('cache' => false);
+    }
+    if (isset($config['cache']) && $config['cache'] !== false) {
+        /* expand twig cache path */
+        $config['cache'] = tr($config['cache']);
+    }
+
+    $twig = new Twig_Environment($twig_loader, $config);
+
+    /* add custom functions to twig here */
+    $twig->addFunction(new Twig_Function('t', function ($key) {return tr('{tr:' . $key . '}');}));
+    $twig->addFunction(new Twig_Function('tr', 'tr'));
+    $twig->addFunction(new Twig_Function('lang', function () {return cfg(['setup', 'lang']);}));
+    $twig->addFunction(new Twig_Function('css', function () {return [];}));
+    $twig->addFunction(new Twig_Function('javascript', function () {return [];}));
+    $twig->addFunction(new Twig_Function('route', function () {return '';}));
+    $twig->addFunction(new Twig_Function('authorize', function () {return false;}));
+
+    return $twig;
+}
+
 function twig($template, $params = [])
 {
-    
+    $twig = twig_init();
+    return $twig->render($template, $params);
 }
