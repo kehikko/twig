c5d24065d446447b2cb8b9c44c8e54ed1ed3b7e3
Antti Partanen <aehparta@iki.fi>
2018-12-17 08:30:15 +0200

    small rename

--- twig.php
+++ twig.php
@@ -7,10 +7,10 @@ function twig_init(array $extra_paths = [])
         return $twig;
     }
 
-    $templates_vendor = tool_system_find_files(['views'], [cfg(['paths', 'vendor'])], 2, true);
-    $templates_routes = tool_system_find_files(['views'], [cfg(['paths', 'routes'])], 1, true);
+    $templates_vendor = tool_system_find_files(['views'], [cfg(['path', 'vendor'])], 2, true);
+    $templates_routes = tool_system_find_files(['views'], [cfg(['path', 'routes'])], 1, true);
     $templates        = array_merge($templates_vendor, $templates_routes, $extra_paths);
-    array_unshift($templates, cfg(['paths', 'views']));
+    array_unshift($templates, cfg(['path', 'views']));
 
     $twig_loader = new Twig_Loader_Filesystem($templates);
     $config      = cfg('twig');
